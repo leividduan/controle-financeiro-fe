eaafd0328f4d4c51d13a1390663703ed206c25b0
Deivid <deivid.cardos@gmail.com>
2023-06-25 15:37:39 -0300

    fix: changes

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -21,6 +21,7 @@ function Login() {
       setError({ field: 'email', message: 'Campo obrigatório.' });
     } else {
       removeError('email');
+      removeError('password');
     }
 
     setEmail(event.currentTarget.value);
@@ -31,6 +32,7 @@ function Login() {
       setError({ field: 'password', message: 'Campo obrigatório.' });
     } else {
       removeError('password');
+      removeError('email');
     }
 
     setPassword(event.currentTarget.value);
@@ -76,7 +78,7 @@ function Login() {
             type="email" 
             name="email" 
             id="email" 
-            placeholder="name@company.com" 
+            placeholder="Ex: joao@company.com" 
             required 
             onChange={handleEmailChange} 
             error={getErrorMessageByFieldName('email')}
@@ -99,7 +101,7 @@ function Login() {
         <div className="mt-0.5">
           <a href="#" className="text-sm font-medium text-primary-600 hover:underline">Esqueci minha senha</a>
         </div>
-        <button type="submit" disabled={!isFormValid} className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:bg-primary-400 cursor-not-allowed">Entrar</button>
+        <button type="submit" disabled={!isFormValid} className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:bg-primary-400 disabled:cursor-not-allowed">Entrar</button>
         <p className="text-sm font-light text-gray-500">
                       Ainda não possui conta? <Link to="/register" className="font-medium text-primary-600 hover:underline">Faça o cadastro!</Link>
         </p>
--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -132,7 +132,7 @@ function Register() {
           />
         </div>
         
-        <button type="submit" disabled={!isFormValid} className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:bg-primary-400 cursor-not-allowed">Finalizar</button>
+        <button type="submit" disabled={!isFormValid} className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:bg-primary-400 disabled:cursor-not-allowed">Finalizar</button>
         <p className="text-sm font-light text-gray-500">
                       Já possui conta? <Link to="/login" className="font-medium text-primary-600 hover:underline">Faça o login!</Link>
         </p>
